0c739653d793b49661b64f1abdaee4f2997fbd44
sikamedia <simonwantao@yahoo.com>
2014-06-06 09:04:14 -0400

    create a new address - works without API api key

--- btcxblockchainapi/btcrpc/views.py
+++ btcxblockchainapi/btcrpc/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework import request
+from btcrpc.btcrpcall import BTCRPCall
 
-# Create your views here.
+btcRPCcall = BTCRPCall()
+
+class BTCGetInfoView(APIView):
+
+    def get(self, request, *args, **kw):
+        
+        result = btcRPCcall.do_getinfo()
+        response = Response(result, status=status.HTTP_200_OK)
+        return response
+
+class BTCGetNewAddress(APIView):
+
+    def get(self, request, *args, **kw):
+        result = btcRPCcall.do_get_new_address()
+        response = Response(result, status=status.HTTP_200_OK)
+        return response
+        
+        
+
+
+
--- btcxblockchainapi/btcxblockchainapi/urls.py
+++ btcxblockchainapi/btcxblockchainapi/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
-from quickstart import views
+#from quickstart import views
+#from btcrpc import views
 
-#from django.contrib import admin
-#admin.autodiscover()
+from quickstart.views import *
+from btcrpc.views import *
+
+
+
+from django.contrib import admin
+admin.autodiscover()
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users',  UserViewSet)
+router.register(r'groups', GroupViewSet)
 #router.register(r'api/v1/status', views.MyRESTView)
 
 urlpatterns = patterns('',
@@ -15,10 +21,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'btcxblockchainapi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    #url(r'^api/v1/status/$',  'quickstart.views.MyRESTView', name="MyRESTView_test")
-    url(r'^api/v1/status/$', views.MyRESTView.as_view(), name="MyRESTView_test")
- )
+    url(r'^api/v1/status/$', MyRESTView.as_view(), name="server_status"),
+    url(r'^api/v1/information/$', BTCGetInfoView.as_view(), name="BTC_Get_Information"),
+    url(r'^api/v1/address/$', BTCGetNewAddress.as_view(), name="BTC_Create_New_Address")
+
+    )
