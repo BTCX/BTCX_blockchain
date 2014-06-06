25fafa3deffdb651e8be2216e03a7ed621511c54
sikamedia <simonwantao@yahoo.com>
2014-06-06 11:23:01 -0400

    refactor the code.

---
+++ btcxblockchainapi/btcrpc/btcrpcall.py
@@ -0,0 +1,16 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
+
+
+btcRPCServer = "http://Ulysseys:8NzhGAbEXoJahLkPpNzmLxHqvQusgYVJWWx1J83y95gQ@127.0.0.1:18332" 
+access = AuthServiceProxy(btcRPCServer)
+
+class BTCRPCall(object):
+
+    def __init__(self):
+        pass
+
+    def do_getinfo(self):
+        return access.getinfo()
+
+    def do_get_new_address(self):
+        return access.getnewaddress();
---
+++ btcxblockchainapi/quickstart/serverstatus.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+class Status(object):
+
+    def __init__(self, status="OK", message="", update=None):
+        self.status = status
+        self.message = message
+        self.update = update
+
+    @property
+    def server_satus(self):
+        return self.status
+
+    @server_satus.setter
+    def server_satus(self, value):
+        self.status = value
+    
+
+    @property
+    def server_message(self):
+        return self.message
+
+    @server_message.setter
+    def server_message(self, value):
+        self.message = value
+
+    @property
+    def server_update(self):
+        return self.update
+
+    @server_update.setter
+    def server_update(self, value):
+        self.update = value
+    
+            
+    
+class ServerStatusSerializer(serializers.Serializer):
+    status = serializers.CharField(max_length=10)
+    message = serializers.CharField(max_length=200)
+    update = serializers.CharField(max_length=15)
+
+
+class ServerStatus(object):
+    def __init__(self):
+        pass
+
+    
+    def do_work(self):
+        result =  ((1,2,3, ), (4,5,6,)) # final result
+        return result
+
