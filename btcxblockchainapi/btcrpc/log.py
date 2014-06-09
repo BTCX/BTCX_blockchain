298768f70ca0e4623c39edb1d239b6d02d9ecb05
sikamedia <simonwantao@yahoo.com>
2014-06-09 13:57:54 -0400

    add log.py

---
+++ btcxblockchainapi/btcrpc/log.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+def get_log(log_name):
+    
+    log = logging.getLogger(log_name)
+    log.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    return log
