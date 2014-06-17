908b10cb893d6f8c1592603483483cb35188ed9e
sikamedia <simonwantao@yahoo.com>
2014-06-17 13:24:37 -0400

    refactoring and init unit tests

---
+++ btcxblockchainapi/btcrpc/test/testaddressreceive.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from btcrpc import btcrpcall
+
+class BTCRPCTestCase(TestCase):
+    def setUp(self):
+        pass
+
+
+    
+    def test_address_receive_btc(self):
+        pass
+
+
+    def test_address_receive_btc_with_txid(self):
+        pass 
