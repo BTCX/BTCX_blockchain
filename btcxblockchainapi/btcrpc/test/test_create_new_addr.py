33f4ef1d636568c6bbabbb83a94662d56b3d200a
sikamedia <simonwantao@yahoo.com>
2014-06-28 18:21:19 -0400

    add test for creating new address and add a rpc function call for getreceivedbyaddress

---
+++ btcxblockchainapi/btcrpc/test/test_create_new_addr.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from btcrpc import btcrpcall
+import unittest
+
+class BTCRPCTestCase(TestCase):
+
+    def setUp(self):
+        self.btcRPCcall = btcrpcall.BTCRPCall()
+
+    def test_create_new_address(self):
+        for x in xrange(1, 111):
+            new_address = self.btcRPCcall.do_get_new_address()
+            print new_address
+            
