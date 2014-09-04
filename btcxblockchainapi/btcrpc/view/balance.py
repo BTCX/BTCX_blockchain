c66a60cc24dae36a5d09ffca9f5ae5641a6e9e82
Tao Wan <tao.wan@quinyx.com>
2014-09-04 16:06:08 +0200

    refactoring the name variable.

--- btcxblockchainapi/btcrpc/view/balance.py
+++ btcxblockchainapi/btcrpc/view/balance.py
@@ -19,7 +19,7 @@ class GetBalanceView(APIView):
         log.info(request.is_secure())
         btc_rpc_call = BTCRPCCall(wallet="send")
         #check is testnet or not
-        test_net = constantutil.check_service_is_test_net(btc_rpc_call)
+        is_test_net = constantutil.check_service_is_test_net(btc_rpc_call)
         serializer = balance.GetBalancePostParametersSerializer(data=request.DATA)
 
         if serializer.is_valid():
@@ -29,12 +29,12 @@ class GetBalanceView(APIView):
                 balance_output = btc_rpc_call.get_balance(account=serializer.data["address"])
                 log.info(balance_output)
                 balance_output_response = balance.GetBalanceResponse(balance=float(balance_output),
-                                                                     message="", test=test_net)
+                                                                     message="", test=is_test_net)
                 balance_output_response_serializer = balance.GetBalanceResponseSerializer(balance_output_response)
                 return Response(data=balance_output_response_serializer.data, status=status.HTTP_201_CREATED)
             else:
                 balance_output_response = balance.GetBalanceResponse(balance=0.0,
-                                                                     message="Address is not valid", test=test_net)
+                                                                     message="Address is not valid", test=is_test_net)
                 balance_output_response_serializer = balance.GetBalanceResponseSerializer(balance_output_response)
                 return Response(data=balance_output_response_serializer.data, status=status.HTTP_400_BAD_REQUEST)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
