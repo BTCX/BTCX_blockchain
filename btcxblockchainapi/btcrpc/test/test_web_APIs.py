57849ec3506e6eabe544a3b651e4ad7863256d57
sikamedia <simonwantao@yahoo.com>
2014-09-19 19:46:12 +0200

    add rest Client test.

--- btcxblockchainapi/btcrpc/test/test_web_APIs.py
+++ btcxblockchainapi/btcrpc/test/test_web_APIs.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from btcrpc.test.test_settings import USER_NAME, PASSWORD, FROM_ACCOUNT
 from btcrpc.utils.log import get_log
 from django.test import TestCase
 
@@ -13,3 +16,23 @@ class BTCRPCTestCase(TestCase):
 
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.client = APIClient()
+        log.info("login...")
+        log.info(USER_NAME)
+        log.info(PASSWORD)
+        self.user = User.objects.create_superuser(USER_NAME, email='testuser@test.com', password=PASSWORD)
+        self.user.save()
+        self.login_client = self.client.login(username=USER_NAME, password=PASSWORD)
+        log.info(self.login_client)
+
+    def tearDown(self):
+        log.info("log out...")
+        #self.client.logout()
+
+    def get_balance_test(self):
+        response = self.client.post('/api/v1/balance/', {"currency": "btc", "address": FROM_ACCOUNT}, format='json')
+        log.info(response.content)
+
+
+
+
--- btcxblockchainapi/btcxblockchainapi/settings.py
+++ btcxblockchainapi/btcxblockchainapi/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGINATE_BY': 10
+    'PAGINATE_BY': 10,
 }
 
 
