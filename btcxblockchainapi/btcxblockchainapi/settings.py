95f2f8aed84c07f35f5ea11e47ae75f9f9a8438c
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-02-12 14:27:46 +0100

    Added allowed hosts.

--- btcxblockchainapi/btcxblockchainapi/settings.py
+++ btcxblockchainapi/btcxblockchainapi/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 # Application definition
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'btcrpc',
-    'commonapi',
+    'commonapi'
     # 'sslserver',
 )
 
