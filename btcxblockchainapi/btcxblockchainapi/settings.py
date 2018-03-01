a055eb43d4f468b0b9daae5f402ccbf82e685889
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-03-01 13:44:59 +0100

    Edited settings for production mode.

--- btcxblockchainapi/btcxblockchainapi/settings.py
+++ btcxblockchainapi/btcxblockchainapi/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret'
-SECRET_KEY = 'qe$apa%^*h+g5viw&l%=$zoxb-uijiqxbusj^t=xg+6(*na9tl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
