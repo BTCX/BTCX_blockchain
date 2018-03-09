adb21bb76c1f9477ca3b1928c1dbcfb86ed2db06
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-03-09 19:50:20 +0100

    Changed so that bitcoin cash addresses needs to start with "bchtest" or "bitcoincash"

--- btcxblockchainapi/btcrpc/utils/rpc_calls/bitcoin_cash_rpc.py
+++ btcxblockchainapi/btcrpc/utils/rpc_calls/bitcoin_cash_rpc.py
@@ -8,3 +8,9 @@ class BitcoinCashRpc(PythonBitcoinRpc):
   def set_tx_fee(self, amount):
     #Since we want to use the fee suggested by the node software, we don't make a RPC call to manually set the fee.
     return True
+
+  def do_validate_address(self, address=""):
+    if(address.startswith("bchtest") or address.startswith("bitcoincash")):
+      return self.access.validateaddress(address)
+    else:
+      return {"isvalid": False}
