f9af23985df3b6428de04dde368063697a180363
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-03-09 14:52:59 +0100

    Changed rpc call for Litcoin and Bitoin Cash, to use the suggested txfee by the node instead of the fee set by the request.

--- btcxblockchainapi/btcrpc/utils/rpc_calls/bitcoin_cash_rpc.py
+++ btcxblockchainapi/btcrpc/utils/rpc_calls/bitcoin_cash_rpc.py
@@ -4,3 +4,7 @@ from btcrpc.utils.rpc_calls.python_bitcoinrpc import PythonBitcoinRpc
 class BitcoinCashRpc(PythonBitcoinRpc):
   def __init__(self, wallet, currency="bch"):
     super(BitcoinCashRpc, self).__init__(wallet=wallet, currency=currency)
+
+  def set_tx_fee(self, amount):
+    #Since we want to use the fee suggested by the node software, we don't make a RPC call to manually set the fee.
+    return True
--- btcxblockchainapi/btcrpc/utils/rpc_calls/litecoin_rpc.py
+++ btcxblockchainapi/btcrpc/utils/rpc_calls/litecoin_rpc.py
@@ -4,3 +4,8 @@ from btcrpc.utils.rpc_calls.python_bitcoinrpc import PythonBitcoinRpc
 class LitecoinRpc(PythonBitcoinRpc):
   def __init__(self, wallet, currency="bch"):
     super(LitecoinRpc, self).__init__(wallet=wallet, currency=currency)
+
+
+  def set_tx_fee(self, amount):
+    #Since we want to use the fee suggested by the node software, we don't make a RPC call to manually set the fee.
+    return True
