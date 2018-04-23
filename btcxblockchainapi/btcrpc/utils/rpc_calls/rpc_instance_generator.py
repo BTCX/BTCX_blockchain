d701dd542fcf8dbc7956ada9c08d90378fc59140
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-04-23 14:15:52 +0200

    Removed code that creates an instance of every rpc instance.

--- btcxblockchainapi/btcrpc/utils/rpc_calls/rpc_instance_generator.py
+++ btcxblockchainapi/btcrpc/utils/rpc_calls/rpc_instance_generator.py
@@ -6,8 +6,18 @@ from btcrpc.utils.rpc_calls.python_bitcoinrpc import PythonBitcoinRpc
 class RpcGenerator(object):
   @staticmethod
   def get_rpc_instance(wallet, currency):
-    return {
-      'btc': BitcoinRpc(wallet,currency),
-      'bch': BitcoinCashRpc(wallet,currency),
-      'ltc': LitecoinRpc(wallet,currency)
-    }.get(currency, PythonBitcoinRpc(wallet,currency))
+    if currency == 'btc':
+      return BitcoinRpc(wallet, currency)
+    elif currency == 'bch':
+      return BitcoinCashRpc(wallet, currency)
+    elif currency == 'ltc':
+      return LitecoinRpc(wallet, currency)
+    else:
+      return PythonBitcoinRpc(wallet, currency)
+
+    #Removed code that creates an instance of every rpc instance.
+    # return {
+    #   'btc': BitcoinRpc(wallet,currency),
+    #   'bch': BitcoinCashRpc(wallet,currency),
+    #   'ltc': LitecoinRpc(wallet,currency)
+    # }.get(currency, PythonBitcoinRpc(wallet,currency))
