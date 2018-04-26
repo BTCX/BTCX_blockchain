3ef21ca95c175e0cd88fcaa6b8beeb0b95897eb7
Viktor Tigerström <viktortigerstrom@hotmail.com>
2018-04-26 19:04:34 +0200

    Added endpoint timer to /validate endpoint.

--- btcxblockchainapi/btcrpc/view/validate_address.py
+++ btcxblockchainapi/btcrpc/view/validate_address.py
@@ -14,6 +14,8 @@ from socket import error as socket_error
 from btcrpc.utils.log import get_log, log_info, log_error
 from btcrpc.utils.rpc_calls.rpc_instance_generator import RpcGenerator
 from btcrpc.utils.chain_enum import ChainEnum
+from btcrpc.utils.endpoint_timer import EndpointTimer
+import requests
 
 log = get_log("CheckWalletsBalance view")
 
@@ -23,6 +25,7 @@ yml_config = ConfigFileReader()
 class ValidateAddress(APIView):
 
     def post(self, request):
+        endpoint_timer = EndpointTimer()
         log_info(log, "Request data", request.data)
         chain = ChainEnum.UNKNOWN
         post_serializers = validate_address.ValidateAddressPostParametersSerializer(data=request.data)
@@ -42,6 +45,7 @@ class ValidateAddress(APIView):
                     wallet = walletJSON["wallet_name"]
                     rpc_call = RpcGenerator.get_rpc_instance(wallet=wallet, currency=currency)
                     log_info(log, "RPC instance class", rpc_call.__class__.__name__)
+                    endpoint_timer.validate_is_within_timelimit()
 
                     address_validation = rpc_call.do_validate_address(address=address)
                     log_info(log, "Address validation", address_validation)
@@ -49,6 +53,7 @@ class ValidateAddress(APIView):
                     if address_validation["isvalid"]:
                         chain = constantutil.check_service_chain(rpc_call)
                         log_info(log, "Chain", chain.value)
+                        endpoint_timer.validate_is_within_timelimit()
                         is_valid = True
                     else:
                         continue
@@ -94,6 +99,32 @@ class ValidateAddress(APIView):
                     log_error,
                     error_message,
                     serr,
+                    is_valid=is_valid,
+                    is_mine=is_mine,
+                    part_of_wallet=part_of_wallet,
+                    address=address,
+                    chain=chain,
+                    error=1,
+                    error_message=error_message)
+            except requests.Timeout as ex:
+                error_message = "The request timed out. Message from exception: " + str(ex)
+                validate_address_response = self.create_validate_address_response_and_log(
+                    log_error,
+                    error_message,
+                    ex,
+                    is_valid=is_valid,
+                    is_mine=is_mine,
+                    part_of_wallet=part_of_wallet,
+                    address=address,
+                    chain=chain,
+                    error=1,
+                    error_message=error_message)
+            except BaseException as ex:
+                error_message = "An exception was raised. Error message: " + str(ex)
+                validate_address_response = self.create_validate_address_response_and_log(
+                    log_error,
+                    error_message,
+                    ex,
                     is_valid=is_valid,
                     is_mine=is_mine,
                     part_of_wallet=part_of_wallet,
